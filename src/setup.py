d06a043a0e75675aa2bfd7055d44b60d954df529
Sheena Kapoor <sheenak@andrew.cmu.edu>
2022-04-14 23:27:10 +0000

    modified setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name="weatherfc",
+    name="weather",
     version="0.0.1",
     description="weather forecasting",
     maintainer="Sheena Kapoor",
     maintainer_email="sheenak@andrew.cmu.edu",
     license="GPL",
-    packages=["weatherfc"],
-    scripts=["weatherfc/weatherfc.py", "weatherfc/main.py"],
+    packages=["weather"],
+    scripts=["weather/weather.py", "weather/test_weather.py"],
     setup_requires=[],
     data_files=["LICENSE"],
     # install_requires=[],
--- src/zipcode.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-# Taking input from user for Zip Code, displaying it
-zipcode = input("User's US Post Office ZIP Code: ")
-print(f"ZIP Code: {zipcode} \n")
