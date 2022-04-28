7b369e146f2210bebd7070fd549e35db7947b3b0
Sheena Kapoor <sheenak@andrew.cmu.edu>
2022-04-28 06:29:10 +0000

    add license, setup.py again

---
+++ setup.py
@@ -0,0 +1,30 @@
+"""Install package."""
+# pylint: disable=duplicate-code
+
+from setuptools import setup
+
+setup(
+    name="weather",
+    version="0.0.1",
+    description="weather forecasting",
+    maintainer="Sheena Kapoor",
+    maintainer_email="sheenak@andrew.cmu.edu",
+    license="GPL",
+    packages=["src/weatherfc"],
+    scripts=[
+        "src/weatherfc/weather.py",
+        "src/weatherfc/test_weather.py",
+        "src/weatherfc/example_weather.py",
+        "src/weatherfc/bin/wfc.py",
+    ],
+    setup_requires=[],
+    data_files=["LICENSE"],
+    install_requires=[
+        "requests",
+    ],
+    long_description="""\
+Weather Description
+==============
+Handy scripts for the weather forecasting project.
+      """,
+)
